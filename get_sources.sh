ec9f289e6ceb7711eeac2285ecc3ea5663b97158
Jonathan Underwood <jonathan.underwood@gmail.com>
2017-10-28 14:49:37 +0100

    Add script for retrieving sources

---
+++ get_sources.sh
@@ -0,0 +1,9 @@
+COOKIE="oraclelicense=accept-securebackup-cookie"
+
+U=152
+B=16
+HASH=aa0333dd3019491ca4f6ddbe78cdb6d0
+LOC=http://download.oracle.com/otn-pub/java/jdk/8u$U-b$B/$HASH
+
+#curl -LJO -b "$COOKIE" $LOC/jdk-8u$U-linux-i586.tar.gz
+curl -LJO -b "$COOKIE" $LOC/jdk-8u$U-linux-x64.tar.gz
